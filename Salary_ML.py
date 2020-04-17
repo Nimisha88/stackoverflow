da4d9f99b6197a2186d2c452507c40b91919f666
Nimisha Viraj <nimishaviraj@Nimishas-MBP-2.home>
2020-04-17 19:56:17 -0400

    Comments added for ease of understanding

--- Salary_ML.py
+++ Salary_ML.py
@@ -1,3 +1,5 @@
+# Imports
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,6 +7,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
 import seaborn as sns
+
+# Functions
 
 def clean_data(df):
     '''
